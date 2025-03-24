7e18112483b46ddfbe6ed08ac13a17ef6b6236f1
Jaques Strydom <jaques.strydom@gmail.com>
2025-03-24 11:35:57 +0200

    Added .Net Framework Project Generation

--- GenerateNewSolution.ps1
+++ GenerateNewSolution.ps1
@@ -1,7 +1,13 @@
+# Add a parameter for the .NET version
+param(
+    [string]$frameworkVersion = "net8.0",  # Default to .NET 8.0 if not specified
+    [switch]$isNetFramework = $false       # Flag to indicate if this is .NET Framework
+)
+
 # Define the parameters
-$solutionName = "MyApplication"
-$solutionPath = ".\$solutionName\"
-$sourcePath = ".\$solutionName\src\"
+$solutionName = "MassParameterDaemon"
+$solutionPath = "..\$solutionName\"
+$sourcePath = "..\$solutionName\src\"
 $readmeFilePath = "$($solutionPath)\README.md"
 $dataProjectName = "$($solutionName).Data"
 $repositoryProjectName = "$($solutionName).Repository"
@@ -14,49 +20,153 @@ $blazorProjectName = "$($solutionName).UI.Blazor"
 $dtoProjectName = "$($solutionName).Dto"
 $commonProjectName = "$($solutionName).Common"
 $iocProjectName = "$($solutionName).Ioc"
-#$dbProjectName = "$($solutionName).Db"
 $testProjectName = "$($solutionName).Test"
+
+# Function to create a .NET Framework project
+function New-NetFrameworkProject {
+    param (
+        [string]$projectName,
+        [string]$projectPath,
+        [string]$projectType,
+        [string]$frameworkVersion
+    )
+    
+    # Create project directory if it doesn't exist
+    New-Item -ItemType Directory -Force -Path $projectPath | Out-Null
+    
+    Write-Output "Creating $projectType project: $projectName (.NET Framework $frameworkVersion)"
+    
+    # Create the .csproj file
+    $csprojContent = @"
+<?xml version="1.0" encoding="utf-8"?>
+<Project ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <Import Project="\`$(MSBuildExtensionsPath)\`$(MSBuildToolsVersion)\Microsoft.Common.props" Condition="Exists('\`$(MSBuildExtensionsPath)\`$(MSBuildToolsVersion)\Microsoft.Common.props')" />
+  <PropertyGroup>
+    <Configuration Condition=" '\`$(Configuration)' == '' ">Debug</Configuration>
+    <Platform Condition=" '\`$(Platform)' == '' ">AnyCPU</Platform>
+    <ProjectGuid>{$(New-Guid)}</ProjectGuid>
+    <OutputType>$projectType</OutputType>
+    <RootNamespace>$projectName</RootNamespace>
+    <AssemblyName>$projectName</AssemblyName>
+    <TargetFrameworkVersion>v$frameworkVersion</TargetFrameworkVersion>
+    <FileAlignment>512</FileAlignment>
+    <AutoGenerateBindingRedirects>true</AutoGenerateBindingRedirects>
+    <Deterministic>true</Deterministic>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '\`$(Configuration)|\`$(Platform)' == 'Debug|AnyCPU' ">
+    <DebugSymbols>true</DebugSymbols>
+    <DebugType>full</DebugType>
+    <Optimize>false</Optimize>
+    <OutputPath>bin\Debug\</OutputPath>
+    <DefineConstants>DEBUG;TRACE</DefineConstants>
+    <ErrorReport>prompt</ErrorReport>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '\`$(Configuration)|\`$(Platform)' == 'Release|AnyCPU' ">
+    <DebugType>pdbonly</DebugType>
+    <Optimize>true</Optimize>
+    <OutputPath>bin\Release\</OutputPath>
+    <DefineConstants>TRACE</DefineConstants>
+    <ErrorReport>prompt</ErrorReport>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+  <ItemGroup>
+    <Reference Include="System" />
+    <Reference Include="System.Core" />
+    <Reference Include="System.Xml.Linq" />
+    <Reference Include="System.Data.DataSetExtensions" />
+    <Reference Include="Microsoft.CSharp" />
+    <Reference Include="System.Data" />
+    <Reference Include="System.Net.Http" />
+    <Reference Include="System.Xml" />
+  </ItemGroup>
+  <ItemGroup>
+    <Compile Include="Properties\AssemblyInfo.cs" />
+  </ItemGroup>
+  <Import Project="\`$(MSBuildToolsPath)\Microsoft.CSharp.targets" />
+</Project>
+"@
+    
+    # Create AssemblyInfo.cs
+    $assemblyInfoContent = @"
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("$projectName")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("$projectName")]
+[assembly: AssemblyCopyright("Copyright \$(Get-Date -Format yyyy)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("\$(New-Guid)")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+"@
+    
+    # Create the project structure
+    New-Item -ItemType Directory -Force -Path "$projectPath\Properties" | Out-Null
+    Set-Content -Path "$projectPath\$projectName.csproj" -Value $csprojContent
+    Set-Content -Path "$projectPath\Properties\AssemblyInfo.cs" -Value $assemblyInfoContent
+}
 
 Write-Output "create solution"
 # Create the solution
 dotnet new sln --output $solutionPath
 
-Write-Output "Create Projects"
-# Create the projects
-dotnet new classlib -n $dataProjectName -o "$sourcePath\$dataProjectName"
-dotnet new classlib -n $repositoryProjectName -o "$sourcePath\$repositoryProjectName"
-dotnet new classlib -n $libraryProjectName -o "$sourcePath\$libraryProjectName"
-dotnet new webapi -n $apiProjectName -o "$sourcePath\$apiProjectName"
-dotnet new classlib -n $gatewayProjectName -o "$sourcePath\$gatewayProjectName"
-dotnet new console -n $consoleProjectName -o "$sourcePath\$consoleProjectName"
-dotnet new wpf -n $wpfProjectName -o "$sourcePath\$wpfProjectName"
-dotnet new blazorserver -n $blazorProjectName -o "$sourcePath\$blazorProjectName"
-dotnet new classlib -n $dtoProjectName -o "$sourcePath\$dtoProjectName"
-dotnet new classlib -n $commonProjectName -o "$sourcePath\$commonProjectName"
-dotnet new classlib -n $iocProjectName -o "$sourcePath\$iocProjectName"
-#dotnet new sqlserver -n $dbProjectName -o "$sourcePath\$dbProjectName"
-dotnet new xunit -n $testProjectName -o "$sourcePath\$testProjectName"
+Write-Output "`nCreate Projects"
+# Create the projects based on framework type
+if ($isNetFramework) {
+    Write-Output "Creating .NET Framework projects..."
+    # Extract version number (e.g., "net47" -> "4.7")
+    $frameworkNum = $frameworkVersion -replace 'net', ''
+    $dotNetVersion = "$($frameworkNum[0]).$($frameworkNum.Substring(1))"
+    
+    # Create .NET Framework projects
+    New-NetFrameworkProject -projectName $dataProjectName -projectPath "$sourcePath\$dataProjectName" -projectType "Library" -frameworkVersion $dotNetVersion
+    New-NetFrameworkProject -projectName $repositoryProjectName -projectPath "$sourcePath\$repositoryProjectName" -projectType "Library" -frameworkVersion $dotNetVersion
+    New-NetFrameworkProject -projectName $libraryProjectName -projectPath "$sourcePath\$libraryProjectName" -projectType "Library" -frameworkVersion $dotNetVersion
+    New-NetFrameworkProject -projectName $gatewayProjectName -projectPath "$sourcePath\$gatewayProjectName" -projectType "Library" -frameworkVersion $dotNetVersion
+    New-NetFrameworkProject -projectName $dtoProjectName -projectPath "$sourcePath\$dtoProjectName" -projectType "Library" -frameworkVersion $dotNetVersion
+    New-NetFrameworkProject -projectName $commonProjectName -projectPath "$sourcePath\$commonProjectName" -projectType "Library" -frameworkVersion $dotNetVersion
+    New-NetFrameworkProject -projectName $iocProjectName -projectPath "$sourcePath\$iocProjectName" -projectType "Library" -frameworkVersion $dotNetVersion
+    New-NetFrameworkProject -projectName $consoleProjectName -projectPath "$sourcePath\$consoleProjectName" -projectType "Exe" -frameworkVersion $dotNetVersion
+    
+    Write-Output "Note: Web API, Blazor, and some other project types are not supported in .NET Framework $dotNetVersion"
+    Write-Output "Please create these projects manually in Visual Studio if needed."
+} else {
+    Write-Output "Creating .NET Core/5+ projects..."
+    # Create .NET Core/5+ projects as before
+    foreach ($project in @(
+        @{Name=$dataProjectName; Type="classlib"},
+        @{Name=$repositoryProjectName; Type="classlib"},
+        @{Name=$libraryProjectName; Type="classlib"},
+        @{Name=$apiProjectName; Type="webapi"},
+        @{Name=$gatewayProjectName; Type="classlib"},
+        @{Name=$consoleProjectName; Type="console"},
+        @{Name=$wpfProjectName; Type="wpf"},
+        @{Name=$blazorProjectName; Type="blazorserver"},
+        @{Name=$dtoProjectName; Type="classlib"},
+        @{Name=$commonProjectName; Type="classlib"},
+        @{Name=$iocProjectName; Type="classlib"},
+        @{Name=$testProjectName; Type="xunit"}
+    )) {
+        Write-Output "Creating $($project.Type) project: $($project.Name) ($frameworkVersion)"
+        dotnet new $project.Type -n $project.Name -o "$sourcePath\$($project.Name)" --framework $frameworkVersion
+    }
+}
 
 Write-Output "Add Projects to solution"
-# Add the projects to the solution
-# dotnet sln "$sourcePath\$solutionName.sln" add myapp\myapp.csproj
-dotnet sln "$solutionPath$solutionName.sln" add `
-    "$sourcePath\$dataProjectName\$dataProjectName.csproj" `
-    "$sourcePath\$repositoryProjectName\$repositoryProjectName.csproj" `
-    "$sourcePath\$libraryProjectName\$libraryProjectName.csproj" `
-    "$sourcePath\$apiProjectName\$apiProjectName.csproj" `
-    "$sourcePath\$gatewayProjectName\$gatewayProjectName.csproj" `
-    "$sourcePath\$consoleProjectName\$consoleProjectName.csproj" `
-    "$sourcePath\$wpfProjectName\$wpfProjectName.csproj" `
-    "$sourcePath\$blazorProjectName\$blazorProjectName.csproj" `
-    "$sourcePath\$dtoProjectName\$dtoProjectName.csproj" `
-    "$sourcePath\$commonProjectName\$commonProjectName.csproj" `
-    "$sourcePath\$iocProjectName\$iocProjectName.csproj" `
-#    "$sourcePath\$dbProjectName\$dbProjectName.sqlproj" `
-    "$sourcePath\$testProjectName\$testProjectName.csproj"
+# Add all existing .csproj files to the solution
+Get-ChildItem -Path $sourcePath -Recurse -Filter "*.csproj" | ForEach-Object {
+    dotnet sln "$solutionPath$solutionName.sln" add $_.FullName
+}
 
 Write-Output "Creating README.md file"
-New-Item -Path $readmeFilePath -ItemType File
+New-Item -Path $readmeFilePath -ItemType File -Force
 
 $readmeContent = "# $solutionName"
 Set-Content -Path $readmeFilePath -Value $readmeContent
--- README.md
+++ README.md
@@ -16,5 +16,34 @@ $solutionPath = ".\subFolder\$solutionName\"
 $sourcePath = ".\subFolder\$solutionName\"
 ```
 
+## Running Script
+We can generate either a .NET Framework or .NET Core solution.
+If the framework is not specified, net8.0 will be used.
+``` powershell
+.\GenerateNewSolution.ps1 -frameworkVersion "net6.0"
+.\GenerateNewSolution.ps1 -frameworkVersion "net47" -isNetFramework
+```
+
+The project type will be displayed during project generation.
+After the project has been generated, we can run the following command to check
+
+``` powershell
+Get-Content "path\to\project\projectname.csproj"
+```
+
+## Framework Supported Versions
+``` powershell
+net45 (.NET Framework 4.5)
+net451 (.NET Framework 4.5.1)
+net452 (.NET Framework 4.5.2)
+net46 (.NET Framework 4.6)
+net461 (.NET Framework 4.6.1)
+net462 (.NET Framework 4.6.2)
+net47 (.NET Framework 4.7)
+net471 (.NET Framework 4.7.1)
+net472 (.NET Framework 4.7.2)
+net48 (.NET Framework 4.8)
+```
+
 ## TODO
 Determine how to generate a SQL Server Database project.
